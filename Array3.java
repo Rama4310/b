4fed1beab02cb67154c12ac573f5aa2567c8d9ab
rama <ramarao4310@gmail.com>
2024-12-07 08:35:38 +0530

    Add files via upload

---
+++ Array3.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+
+
+/**
+ * @author YOGESH
+ *
+ */
+public class Array3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int []int1 = {1,2,34,5,00,8}; // integer type of array
+		System.out.println(int1.length);//print the length of array
+
+	}
+
+}
